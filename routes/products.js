484ceef45f8fd8cad66cae62c824d3da300b9bd4
Brian Wilson <bmwilson93@gmail.com>
2024-10-20 23:27:16 -0600

    added a route /new to send the 3 most recently added products

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,21 @@
 
 const express = require("express"),
       router = express.Router();
-const { getAllProducts, getProduct } = require('../database/db-products.js');
+const { getAllProducts, getProduct, getNewProducts } = require('../database/db-products.js');
 
 router.get('/', async (req, res) => {
   const allProducts = await getAllProducts();
   res.json(allProducts);
+});
+
+
+router.get('/new', async (req, res) => {
+  const newProducts = await getNewProducts();
+  res.json(newProducts);
 })
-.get('/:id', async (req, res) => {
+
+
+router.get('/:id', async (req, res) => {
   const product = await getProduct(req.params.id);
   res.json(product);
 });
