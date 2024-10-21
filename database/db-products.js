5c0c132182601d1d59a38fc0422a2ac24d935d07
Brian Wilson <bmwilson93@gmail.com>
2024-10-20 23:26:32 -0600

    added a function to return the 3 most recently added products from the database

--- database/db-products.js
+++ database/db-products.js
@@ -5,9 +5,20 @@ const getAllProducts = async () => {
   return result.rows;
 }
 
+// returned the 3 most recently added products
+const getNewProducts = async () => {
+  const result = await db.query('SELECT * FROM products ORDER BY id DESC');
+  console.log(result.rows);
+  const newProducts = [];
+  for (let i = 0; i < 3; i++) {
+    newProducts.push(result.rows[i]);
+  }
+  return newProducts;
+}
+
 const getProduct = async (id) => {
   const result = await db.query(`SELECT * FROM products WHERE id='${id}';`);
   return result.rows[0];
 }
 
-module.exports = { getAllProducts, getProduct }
+module.exports = { getAllProducts, getProduct, getNewProducts }
