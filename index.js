9e2e6bb307eb67b027a1961cf4d70dc509b12c74
Brian Wilson <bmwilson93@gmail.com>
2024-10-25 03:33:01 -0600

    changed origin in cors for now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors  = require('cors');
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['POST', 'PUT', 'GET', 'DELETE', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
