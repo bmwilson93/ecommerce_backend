27c6f7cc1498f1d1d9a0c01f6d25aa67bd84bfbb
Brian Wilson <bmwilson93@gmail.com>
2024-11-07 01:50:19 -0700

    changed pool to use connectionString

--- database/db.js
+++ database/db.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  ssl: true,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD
+  // host: process.env.DB_HOST,
+  // ssl: true,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // dialect: 'postgres'
+  connectionString: process.env.DB_CONNECTION_STRING
 });
 
 pool.connect();
