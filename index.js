8f5583a9e1bc4308cca2966869cff7028f96506e
Brian Wilson <bmwilson93@gmail.com>
2024-11-09 06:19:03 -0700

    trying updating cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors  = require('cors');
 app.use(cors({
-  // origin: '*',
+  origin: 'https://ecommerce-frontend-rqru.onrender.com',
   methods: ['POST', 'PUT', 'GET', 'DELETE', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
